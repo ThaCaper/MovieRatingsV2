4109be65826630d01f42194af1f1c5238416bdde
andreas <andreas-cooper@hotmail.com>
2019-11-07 11:12:00 +0100

    mklj

--- XUnitPerformanceTest/XUnitPTest.cs
+++ XUnitPerformanceTest/XUnitPTest.cs
@@ -9,7 +9,11 @@ namespace XUnitPerformanceTest
     {
         private readonly int movieId = 1488844;
         private readonly int MAXTIME = 4;
+<<<<<<< Updated upstream
         readonly MovieRating mr = new MovieRating(@"C:\Users\andre\Documents\GitHub\MovieRatingsV2\ratings.json");
+=======
+        readonly MovieRating mr = new MovieRating(@"C:\Users\Caspe\Documents\GitHub\MovieRatingsV2\ratings.json");
+>>>>>>> Stashed changes
         [Fact]
         public void Test1()
         {
