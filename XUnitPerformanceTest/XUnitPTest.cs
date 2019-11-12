32466c385218b8ae1ec34e1d6b985d57f452baa6
andreas <andreas-cooper@hotmail.com>
2019-11-12 10:01:14 +0100

    Update XUnitPTest.cs

--- XUnitPerformanceTest/XUnitPTest.cs
+++ XUnitPerformanceTest/XUnitPTest.cs
@@ -9,7 +9,7 @@ namespace XUnitPerformanceTest
     {
         private readonly int movieId = 1488844;
 
-        private readonly int MAXTIME = 15;
+        private readonly int MAXTIME = 5;
         readonly MovieRating mr = new MovieRating(@"..\..\..\..\ratings.json");
 
         [Fact]
