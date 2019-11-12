6671e61b21f0bff2d8dd044ad33d75f6da6f7651
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2019-11-12 09:40:00 +0100

    Update MSTestingMethods.cs

--- MSTest/MSTestingMethods.cs
+++ MSTest/MSTestingMethods.cs
@@ -8,11 +8,10 @@ namespace MSTest
     [TestClass]
     public class MSTestingMethods
     {
-<<<<<<< Updated upstream
-        readonly MovieRating mr = new MovieRating(@"C:\Users\andre\Documents\GitHub\MovieRatingsV2\ratings.json");
-=======
+
+        readonly MovieRating mr1 = new MovieRating(@"C:\Users\andre\Documents\GitHub\MovieRatingsV2\ratings.json");
         readonly MovieRating mr = new MovieRating(@"C:\Users\Caspe\Documents\GitHub\MovieRatingsV2\ratings.json");
->>>>>>> Stashed changes
+
 
         //opgave 1
         [DataRow(1, 3)]
